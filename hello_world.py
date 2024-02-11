f4535a15027bb51e82cce7facc6d564fef2c5d30
Rhiannon8108 <144738307+Rhiannon8108@users.noreply.github.com>
2024-02-11 12:12:13 -0700

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -5,7 +5,10 @@ root.title("Desktop Application")
 root.geometry("750x500")
 
 lable = tk.Label(root, width=22, text="Hello World", anchor = 'w')
-lable.confi(font=("Raleway", 16))
+lable.config(font=("Raleway", 16))
 lable.pack(side=tk.TOP, pady = 30)
 
+row = tk.Frame(root)
+label = tk.Label(row, width=22, text="Hello World", anchor='w')
+lable.config(font=("Raleway", 16))
 
