3900f7836981fb8c5f97099507de40ff3ad576e9
Rhiannon8108 <144738307+Rhiannon8108@users.noreply.github.com>
2024-02-12 10:48:29 -0700

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,15 @@
 import tkinter as tk 
 
-root = tk.tk()
+root = tk.Tk()
 root.title("Desktop Application")
 root.geometry("750x500")
 
-lable = tk.Label(root, width=22, text="Hello World", anchor = 'w')
-lable.config(font=("Raleway", 16))
-lable.pack(side=tk.TOP, pady = 30)
+label = tk.Label(root, width=22, text="Hello World", anchor = 'w')
+label.config(font=("Raleway", 16))
+label.pack(side=tk.TOP, pady = 30)
 
 row = tk.Frame(root)
 label = tk.Label(row, width=22, text="Hello World", anchor='w')
 lable.config(font=("Raleway", 16))
 
+root.mainloop() 
